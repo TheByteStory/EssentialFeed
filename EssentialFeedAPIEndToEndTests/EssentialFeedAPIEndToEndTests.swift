8c8a68f23610b9d72a93303301fb766d9a8e729a
Sharanya Abishek <thebytestory@gmail.com>
2020-10-02 08:18:33 +0530

    Extract test setup to a helper method

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -13,22 +13,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData(){
         
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
-        let exp = expectation(description: "Waiting for load completion")
-        
-        var receivedResult : LoadFeedResult?
-        
-        loader.load{ result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 5.0)
-        
-        switch(receivedResult){
+        switch getFeedResult(){
         case .success(items)?:
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             
@@ -51,6 +36,25 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     // MARK :- Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+               let client = URLSessionHTTPClient()
+               let loader = RemoteFeedLoader(url: testServerURL, client: client)
+               
+               let exp = expectation(description: "Waiting for load completion")
+               
+               var receivedResult : LoadFeedResult?
+               
+               loader.load{ result in
+                   receivedResult = result
+                   exp.fulfill()
+               }
+               
+               wait(for: [exp], timeout: 5.0)
+        return receivedResult
+        }
     
     private func expectedItem(at index: Int) -> FeedItem{
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at :index), imageURL: imageURL(at:index))
