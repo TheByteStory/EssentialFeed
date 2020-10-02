1578a60300846773023773c220000f8fa523f05a
Sharanya Abishek <thebytestory@gmail.com>
2020-10-02 13:22:26 +0530

    Add Travis CI config

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -11,51 +11,48 @@ import EssentialFeed
 
 class EssentialFeedAPIEndToEndTests: XCTestCase {
 
-    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData(){
-        
-        switch getFeedResult(){
-        case .success(items)?:
-            XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
-            
-            items.enumerated().forEach{(index, item) in
-                XCTAssertEqual(items[0], expectedItem(at: 0))
-                XCTAssertEqual(items[1], expectedItem(at: 1))
-                XCTAssertEqual(items[2], expectedItem(at: 2))
-                XCTAssertEqual(items[3], expectedItem(at: 3))
-                XCTAssertEqual(items[4], expectedItem(at: 4))
-                XCTAssertEqual(items[5], expectedItem(at: 5))
-                XCTAssertEqual(items[6], expectedItem(at: 6))
-                XCTAssertEqual(items[7], expectedItem(at: 7))
-            }
-        case .failure(error)?:
-            XCTFail("Expected successful feed result, got \(error) instead")
-        case default:
-            XCTFail("Expected successful feed result, got no result instead")
-        }
-        
-    }
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+         
+         switch getFeedResult() {
+         case let .success(items)?:
+             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
+             XCTAssertEqual(items[0], expectedItem(at: 0))
+             XCTAssertEqual(items[1], expectedItem(at: 1))
+             XCTAssertEqual(items[2], expectedItem(at: 2))
+             XCTAssertEqual(items[3], expectedItem(at: 3))
+             XCTAssertEqual(items[4], expectedItem(at: 4))
+             XCTAssertEqual(items[5], expectedItem(at: 5))
+             XCTAssertEqual(items[6], expectedItem(at: 6))
+             XCTAssertEqual(items[7], expectedItem(at: 7))
+
+         case let .failure(error)?:
+             XCTFail("Exoected successful feed result, got \(error) instead")
+
+         default:
+             XCTFail("Expected successful feed result, got no result instead")
+         }
+     }
+
     
     // MARK :- Helpers
     
-    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
-        
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-               let client = URLSessionHTTPClient()
-               let loader = RemoteFeedLoader(url: testServerURL, client: client)
-               trackForMemoryLeaks(client, file: file,line: line)
-               trackForMemoryLeaks(loader, file: file,line: line)
-               let exp = expectation(description: "Waiting for load completion")
-               
-               var receivedResult : LoadFeedResult?
-               
-               loader.load{ result in
-                   receivedResult = result
-                   exp.fulfill()
-               }
-               
-               wait(for: [exp], timeout: 5.0)
-        return receivedResult
-        }
+    private func getFeedResult() -> LoadFeedResult? {
+         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+         let client = URLSessionHTTPClient()
+         let loader = RemoteFeedLoader(url: testServerURL, client: client)
+
+         let exp = expectation(description: "Wait for load completion")
+
+         var receivedResult: LoadFeedResult?
+         loader.load { result in
+             receivedResult = result
+             exp.fulfill()
+         }
+         wait(for: [exp], timeout: 5.0)
+
+         return receivedResult
+     }
+
     
     private func expectedItem(at index: Int) -> FeedItem{
         return FeedItem(id: id(at: index), description: description(at: index), location: location(at :index), imageURL: imageURL(at:index))
