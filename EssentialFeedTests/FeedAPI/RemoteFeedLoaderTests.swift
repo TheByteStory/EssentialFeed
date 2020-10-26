c72db82cf290d6ef8d86d622612954e38e7c061e
Sharanya Abishek <thebytestory@gmail.com>
2020-10-26 16:37:44 +0530

    Rename 'RemoteFeedLoaderTests' to reflect the use case it refers to (Load Feed from Remote)

--- EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/FeedAPI/RemoteFeedLoaderTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 @testable import EssentialFeed
 
-class RemoteFeedLoaderTests : XCTestCase
+class LoadFeedFromRemoteUseCaseTests : XCTestCase
 {
     
     func test_init_doesNotRequestDataFromURL()
