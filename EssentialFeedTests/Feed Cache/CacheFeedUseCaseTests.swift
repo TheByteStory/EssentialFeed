89111ea9324d8b728f7f6b29bf710445bf3153fd
Sharanya Abishek <thebytestory@gmail.com>
2020-10-27 08:58:15 +0530

    Save command requests cache insertion with timestamp on successful deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -12,33 +12,56 @@ import EssentialFeed
 class LocalFeedLoader
 {
     private let store:FeedStore
+    private let currentDate : () -> Date
     
     
-    
-    init(store:FeedStore){
+    init(store:FeedStore, currentDate:@escaping() -> Date){
         self.store = store
+        self.currentDate = currentDate
     }
     
     func save(_ items:[FeedItem])
     {
-        store.deleteCachedFeed()
-    }
+        store.deleteCachedFeed{[unowned self] error in
+            if error == nil
+            {
+                self.store.insert(items, timestamp:self.currentDate())
+            }
+            
+        }
     
+    }
     
 }
 
 class FeedStore
 {
+    typealias DeletionCompletion = (Error?) -> Void
     var insertCallCount = 0
     var deleteCachedFeedCallCount = 0
+    var insertions = [(items:[FeedItem],timestamp:Date)]()
     
-    func deleteCachedFeed()
+    private var deletionCompletions = [DeletionCompletion]()
+    
+    func deleteCachedFeed(completion:@escaping DeletionCompletion)
     {
         deleteCachedFeedCallCount += 1
+        deletionCompletions.append(completion)
     }
     func completeDeletion(with error:Error, at index:Int = 0)
     {
-
+        deletionCompletions[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index:Int = 0)
+    {
+        deletionCompletions[index](nil)
+    }
+    
+    func insert(_ items: [FeedItem],timestamp:Date)
+    {
+        insertCallCount += 1
+        insertions.append((items,timestamp))
     }
 }
 
@@ -48,7 +71,7 @@ class CacheFeedUseCaseTests: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation()
     {
         let (_,store) = makeSUT()
-        let _ = LocalFeedLoader(store:store)
+        //let _ = LocalFeedLoader(store:store,timestamp:Date())
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
@@ -66,24 +89,54 @@ class CacheFeedUseCaseTests: XCTestCase {
     {
         let items = [uniqueItem(),uniqueItem()]
         let (sut,store) = makeSUT()
-        var deletionError = anyNSError()
-        
-        
+        let deletionError = anyNSError()
+
         sut.save(items)
         store.completeDeletion(with: deletionError)
-        
+
         XCTAssertEqual(store.insertCallCount,0)
-        
+
     }
     
     
     
+    //New feed items inserted as cache
+    func test_save_requestsNewCacheInsertionOnSuccessfulDeletion()
+    {
+        let items = [uniqueItem(),uniqueItem()]
+        let (sut,store) = makeSUT()
+
+
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+
+        XCTAssertEqual(store.insertCallCount,1)
+    }
+    
+    //New feed items inserted as cache with timestamp
+    func test_save_requestsNewCacheInsertionWithTimestampOnSuccessfulDeletion()
+       {
+           let timestamp = Date()
+           let items = [uniqueItem(),uniqueItem()]
+           let (sut,store) = makeSUT(currentDate: { timestamp })
+
+
+           sut.save(items)
+           store.completeDeletionSuccessfully()
+
+        XCTAssertEqual(store.insertions.count,1)
+        XCTAssertEqual(store.insertions.first?.items,items)
+        XCTAssertEqual(store.insertions.first?.timestamp,timestamp)
+       }
+    
+
+    
     //MARK :- Helpers
     
-    private func makeSUT(file:StaticString = #file,line:UInt = #line) -> (sut:LocalFeedLoader, store:FeedStore)
+    private func makeSUT(currentDate:@escaping() -> Date = Date.init, file:StaticString = #file,line:UInt = #line) -> (sut:LocalFeedLoader, store:FeedStore)
     {
         let store = FeedStore()
-        let sut = LocalFeedLoader(store:store)
+        let sut = LocalFeedLoader(store:store, currentDate:currentDate)
         trackForMemoryLeaks(store,file:file,line:line)
         trackForMemoryLeaks(sut,file:file,line:line)
         return(sut,store)
