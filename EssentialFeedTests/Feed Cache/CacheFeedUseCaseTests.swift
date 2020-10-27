af77d42aea473d88959033010fa3db8d454dafb4
Sharanya Abishek <thebytestory@gmail.com>
2020-10-27 08:22:15 +0530

    Save command does not request cache insertion on deletion error

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -13,6 +13,8 @@ class LocalFeedLoader
 {
     private let store:FeedStore
     
+    
+    
     init(store:FeedStore){
         self.store = store
     }
@@ -21,20 +23,28 @@ class LocalFeedLoader
     {
         store.deleteCachedFeed()
     }
+    
+    
 }
 
 class FeedStore
 {
+    var insertCallCount = 0
     var deleteCachedFeedCallCount = 0
     
     func deleteCachedFeed()
     {
         deleteCachedFeedCallCount += 1
     }
+    func completeDeletion(with error:Error, at index:Int = 0)
+    {
+
+    }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
 
+    //Cache deletion failure
     func test_init_doesNotDeleteCacheUponCreation()
     {
         let (_,store) = makeSUT()
@@ -42,14 +52,31 @@ class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
+    //Cache deletion successful
     func test_save_requestsCacheDeletion()
     {
-        
-        let (sut,store) = makeSUT()
         let items = [uniqueItem(),uniqueItem()]
+        let (sut,store) = makeSUT()
         sut.save(items)
         XCTAssertEqual(store.deleteCachedFeedCallCount,1)
     }
+    
+    //If cache deletion fails, do not insert new cache
+    func test_save_doesNotRequestCacheInsertionOnDeletionError()
+    {
+        let items = [uniqueItem(),uniqueItem()]
+        let (sut,store) = makeSUT()
+        var deletionError = anyNSError()
+        
+        
+        sut.save(items)
+        store.completeDeletion(with: deletionError)
+        
+        XCTAssertEqual(store.insertCallCount,0)
+        
+    }
+    
+    
     
     //MARK :- Helpers
     
@@ -71,5 +98,10 @@ class CacheFeedUseCaseTests: XCTestCase {
     {
         return URL(string:"https://any-url.com")!
     }
+    
+    private func anyNSError() -> NSError
+       {
+           return NSError(domain: "any error", code: 0)
+       }
 
 }
