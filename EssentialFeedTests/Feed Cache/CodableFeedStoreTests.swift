036a0f145a1dd279f63b89224df57e08100a8d16
Sharanya Abishek <thebytestory@gmail.com>
2020-11-03 13:30:30 +0530

    Retrieving from empty cache twice delivers same empty result (no side effects)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -19,6 +19,7 @@ class CodableFeedStore
 
 class CodableFeedStoreTests: XCTestCase {
 
+    //Show empty on Empty cache - retrieves once
     func test_retrieve_deliversEmptyOnEmptyCache()
     {
     let sut = CodableFeedStore()
@@ -36,4 +37,42 @@ class CodableFeedStoreTests: XCTestCase {
         
         wait(for:[exp], timeout: 1.0)
     }
+    
+    //retrieving cache twice without side effects
+    func test_retrieve_hasNoSideEffectsOnEmptyCache()
+    {
+    let sut = CodableFeedStore()
+    let exp = expectation(description: "Wait for cache retrieval")
+
+        sut.retrieve{ firstResult in
+            sut.retrieve{ secondResult in
+                    switch (firstResult, secondResult) {
+                    case (.empty,.empty):
+                        break
+                    default:
+                        XCTFail("Expected empty result, got \(firstResult) and \(secondResult) instead")
+                    }
+                    exp.fulfill()
+                }
+        }
+        wait(for:[exp], timeout: 1.0)
+    }
+    
+
+    
+//    //With Non Empty cache,override the old value
+//    func test_retrieve_nonEmptyCacheOverridesPreviousValue()
+//    {
+//        let sut = CodableFeedStore()
+//        let exp = expectation(description: "Wait for cahe retrieval")
+//
+//        sut.retrieve{ result in
+//
+//            switch result{
+//            case (notempty):
+//                delete old cache
+//                replace with new cache
+//            }
+//        }
+//    }
 }
