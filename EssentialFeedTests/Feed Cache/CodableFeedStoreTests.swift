6fd454c629ed9963d158cb8f44be49c92db96798
Sharanya Abishek <thebytestory@gmail.com>
2020-11-04 08:19:17 +0530

    Extract duplicate store URL creation into a helper factory method

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -77,9 +77,12 @@ class CodableFeedStoreTests: XCTestCase {
     //Remove artifacts every time - use setup instead of teardown
     override func setUp() {
         super.setUp()
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-        
-        try? FileManager.default.removeItem(at: storeURL)
+        try? FileManager.default.removeItem(at: storeURL())
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        try? FileManager.default.removeItem(at: storeURL())
     }
 
     //Show empty on Empty cache - retrieves once
@@ -151,10 +154,14 @@ class CodableFeedStoreTests: XCTestCase {
     
     private func makeSUT(file : StaticString = #file, line : UInt = #line) -> CodableFeedStore
     {
-        let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
-        let sut = CodableFeedStore(storeURL: storeURL)
+        let sut = CodableFeedStore(storeURL: storeURL())
         trackForMemoryLeaks(sut, file:file, line: line)
         return sut
+    }
+    
+    private func storeURL() -> URL
+    {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
     }
 
 }
